e5431cfc719e77fd9265aed032d082b72d53fcd6
yoonsoyoung <hummingsy@gmail.com>
2023-02-05 14:16:29 +0900

    Docs: Update frontend gitignore

--- backend/scent/scent/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path,include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('scent/api/detail/', include('detail.urls')),
-    path('scent/tests/', include('tests.urls')),
-]
--- backend/scent/tests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'tests'
-urlpatterns = [
-    path('',views.tests),
-    path('result/',views.tests_result),
-    path('getresult/',views.get_result),
-]
